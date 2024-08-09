d7f05114b95220e03ca4fa6312b488e20d00757f
Sasha Poulain <sashapoulain5@gmail.com>
2024-08-09 14:12:04 +0300

    heres my portfolio

---
+++ js/main.js
@@ -0,0 +1,74 @@
+particlesJS('particles-js', {
+   "particles": {
+       "number": {
+           "value": 150,
+           "density": {
+               "enable": true,
+               "value_area": 800
+           }
+       },
+       "color": {
+           "value": "#ffffff"
+       },
+       "shape": {
+           "type": "circle",
+           "stroke": {
+               "width": 0,
+               "color": "#000000"
+           },
+           "polygon": {
+               "nb_sides": 5
+           }
+       },
+       "opacity": {
+           "value": 0.5,
+           "random": true,
+           "anim": {
+               "enable": false,
+               "speed": 1,
+               "opacity_min": 0.1,
+               "sync": false
+           }
+       },
+       "size": {
+           "value": 5,
+           "random": true,
+           "anim": {
+               "enable": false,
+               "speed": 40,
+               "size_min": 0.1,
+               "sync": false
+           }
+       },
+       "line_linked": {
+           "enable": false
+       },
+       "move": {
+           "enable": true,
+           "speed": 2,
+           "direction": "bottom",
+           "random": false,
+           "straight": false,
+           "out_mode": "out",
+           "bounce": false,
+           "attract": {
+               "enable": false,
+               "rotateX": 600,
+               "rotateY": 1200
+           }
+       }
+   },
+   "interactivity": {
+       "detect_on": "canvas",
+       "events": {
+           "onhover": {
+               "enable": false
+           },
+           "onclick": {
+               "enable": false
+           },
+           "resize": true
+       }
+   },
+   "retina_detect": true
+});
